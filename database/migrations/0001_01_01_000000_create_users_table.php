1dfc7bedef25826ede3d629271e0f7d25356a1a6
OlaFouadKotb <engolakotb145@gmail.com>
2024-06-26 16:20:51 +0200

    main

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('userName');
             $table->string('email')->unique();
+            $table->boolean('expired');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone');
